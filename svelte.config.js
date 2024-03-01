b41230bbb52afe65c43b5f3c2a3c0cd3f1f4f6a8
Mohit Chauhan <mohitchauhan0226@gmail.com>
2024-03-02 02:13:52 +0530

    initialized

---
+++ svelte.config.js
@@ -0,0 +1,33 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+		},
+		prerender: {
+			handleHttpError: ({path, referrer, message}) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		}
+	}
+};
+
+export default config;
