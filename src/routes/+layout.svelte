8ea475d61865aec99507499c72d00786533a0cb4
Mohit Chauhan <mohitchauhan0226@gmail.com>
2024-03-02 14:39:26 +0530

    initial-commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 
 
 <script>
-    import { base } from "$app/paths";
     import "../app.css";
     import Header from "../components/Header.svelte";
     import Footer from "../components/Footer.svelte";
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		appDir: "app",
 		adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
 	},
-		preprocess: vitePreprocess()
-
 };
-export default config;
