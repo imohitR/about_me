fc205ea8d6268f20eb9f1a9dce40f83431e20132
Mohit Chauhan <mohitchauhan0226@gmail.com>
2024-03-03 10:57:56 +0530

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,16 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter(
 			{
-				fallback: '404.html',
+				pages: 'build',
+				assets: 'build',
+				fallback: undefined,
+				precompress: false,
+				strict: false,
 			}
 		),
-		paths: {
-			base: '/imohitr.github.io',
-		}
 	},
 };
 export default config;
